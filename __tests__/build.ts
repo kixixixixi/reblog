05b4b888f212cdb6c2f5703fe19f8d2ad4a9cb4c
kixixixixi <kixixixixi@gmail.com>
2020-01-18 00:06:59 +0900

    Fix for test

--- __tests__/build.ts
+++ __tests__/build.ts
@@ -8,6 +8,6 @@ describe('Build', () => {
     const { articleIds, publishArticles } = await build("./__test_articles", "utf-8")
     expect(articleIds).toEqual(_articleIds)
     expect(publishArticles[2020][1].length).toEqual(2)
-    expect(publishArticles[2020][1][0].name).toEqual("test 1")
+    expect([...publishArticles[2020][1]].sort((e1, e2) => e1.id < e2.id ? -1 : 1)[0].name).toEqual("test 1")
   })
 })
