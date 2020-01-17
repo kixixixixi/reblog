bb46e9a9d7a4315ddbca40404f89e914abe204ca
kixixixixi <kixixixixi@gmail.com>
2020-01-17 19:53:26 +0900

    Add test

---
+++ __tests__/build.ts
@@ -0,0 +1,13 @@
+import { build } from "../src/build"
+
+
+const _articleIds = [1, 2]
+
+describe('Build', () => {
+  test('build method', async () => {
+    const { articleIds, publishArticles } = await build("./__test_articles", "utf-8")
+    expect(articleIds).toEqual(_articleIds)
+    expect(publishArticles[2020][1].length).toEqual(2)
+    expect(publishArticles[2020][1][0].name).toEqual("test 1")
+  })
+})
